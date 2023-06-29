521cb76bd5c2bb7d83c801a6cc4c7733b7f5d400
israelsilva282 <israelcostas@hotmail.com>
2023-06-29 13:36:19 -0300

    Adicionado algoritmo LJF

--- scheduler_ljf.c
+++ scheduler_ljf.c
@@ -1,25 +1,85 @@
 #include <stdio.h>
 
-#include "queue.h" // contem funções uteis para filas
-#include "proc.h"  // possui as funções dos processos
-#include "stats.h" // possui as funções de estatisticas 
-#include "utils.h" // possui funções uteis 
+#include "queue.h"
+#include "proc.h"
+#include "stats.h"
+#include "utils.h"
 
-// Utilizando as variáveis globais definidas no 'main'
-extern struct queue * ready;    // fila de aptos
-extern struct queue * ready2;   // segunda fila de aptos
-extern struct queue * blocked;  // fila de bloqueados
-extern struct queue * finished; // fila de finalizados
-// NOTE: essa fila de finalizados é utilizada apenas para
-// as estatisticas finais
+extern struct queue *ready;
+extern struct queue *ready2;
+extern struct queue *blocked;
+extern struct queue *finished;
 
-// variavel global que indica o tempo maximo que um processo pode executar ao todo
 extern int MAX_TIME;
 
-struct proc * scheduler(struct proc * current)
+struct proc *get_higher_time(struct queue *ready)
 {
-    struct proc * selected; 
+    struct proc *p_higher_time = NULL;
+    int max_time = -1;
 
-    return NULL;
+    for (struct proc *p = ready->head; p != NULL; p = p->next)
+    {
+        if (p->remaining_time > max_time)
+        {
+            max_time = p->remaining_time;
+            p_higher_time = p;
+        }
+    }
+
+    if (p_higher_time != NULL)
+    {
+        dequeue_bypid(ready, p_higher_time->pid);
+    }
+
+    return p_higher_time;
 }
 
+struct proc *scheduler(struct proc *current)
+{
+    struct proc *selected;
+
+    /*
+     *   Tratando o processo que está atualmente executando
+     */
+
+    if (current != NULL)
+    {
+        switch (current->state)
+        {
+        case READY:
+            enqueue(ready, current);
+            count_ready_in(current);
+            break;
+
+        case BLOCKED:
+            enqueue(blocked, current);
+            count_blocked_in(current);
+            break;
+
+        case FINISHED:
+            enqueue(finished, current);
+            count_finished_in(current);
+            break;
+
+        default:
+            printf("@@ ERRO no estado de saída do processo %d\n", current->pid);
+        }
+    }
+
+    /*
+     *   Estratégia de seleção de um novo processo para executar
+     */
+
+    if (isempty(ready))
+    {
+        return NULL;
+    }
+
+    selected = get_higher_time(ready);
+
+    count_ready_out(selected);
+
+    selected->state = RUNNING;
+
+    return selected;
+}
