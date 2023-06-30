ae16c042a6d269ad73dada4d5af0f0d144349b96
israelsilva282 <israelcostas@hotmail.com>
2023-06-30 13:30:59 -0300

    Adicionando prio_dynamic

--- scheduler_prio_dynamic.c
+++ scheduler_prio_dynamic.c
@@ -1,25 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+#include "proc.h"
+#include "stats.h"
+#include "utils.h"
 
-#include "queue.h" // contem funções uteis para filas
-#include "proc.h"  // possui as funções dos processos
-#include "stats.h" // possui as funções de estatisticas 
-#include "utils.h" // possui funções uteis 
+// Variáveis globais definidas no 'main'
+extern struct queue *ready;    // fila de processos aptos
+extern struct queue *ready2;   // segunda fila de processos aptos
+extern struct queue *blocked;  // fila de processos bloqueados
+extern struct queue *finished; // fila de processos finalizados
+// OBS: A fila 'finished' é usada apenas para estatísticas finais
 
-// Utilizando as variáveis globais definidas no 'main'
-extern struct queue * ready;    // fila de aptos
-extern struct queue * ready2;   // segunda fila de aptos
-extern struct queue * blocked;  // fila de bloqueados
-extern struct queue * finished; // fila de finalizados
-// NOTE: essa fila de finalizados é utilizada apenas para
-// as estatisticas finais
+extern int MAX_TIME; // tempo máximo que um processo pode executar ao todo
 
-// variavel global que indica o tempo maximo que um processo pode executar ao todo
-extern int MAX_TIME;
-
-struct proc * scheduler(struct proc * current)
+struct proc *get_higher_priority_queue(struct queue *ready, struct queue *ready2)
 {
-    struct proc * selected; 
+    int priority = rand() % 100;
 
-    return NULL;
+    struct proc *selected;
+
+    if (!isempty(ready) && (isempty(ready2) || priority <= 80))
+    {
+        selected = dequeue(ready);
+    }
+    else
+    {
+        selected = dequeue(ready2);
+    }
+
+    return selected;
 }
 
+struct proc *scheduler(struct proc *current)
+{
+    struct proc *selected;
+
+    if (current != NULL)
+    {
+        switch (current->state)
+        {
+        case READY:
+            enqueue(ready2, current);
+            count_ready_in(current);
+            break;
+        case BLOCKED:
+            enqueue(blocked, current);
+            count_blocked_in(current);
+            break;
+        case FINISHED:
+            enqueue(finished, current);
+            count_finished_in(current);
+            break;
+        default:
+            printf("@@ ERRO no estado de saída do processo %d\n", current->pid);
+        }
+    }
+
+    if (isempty(ready) && isempty(ready2))
+    {
+        return NULL;
+    }
+
+    selected = get_higher_priority_queue(ready, ready2);
+
+    count_ready_out(selected); // Atualiza as estatísticas de processos aptos
+    selected->state = RUNNING; // Define o estado do processo selecionado como RUNNING (em execução)
+    return selected;           // Retorna o processo selecionado para ser executado
+}
